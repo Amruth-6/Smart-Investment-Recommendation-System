aa7e6fef1c1851da4f5be34e8274034a33cd1938
Amruth-6 <amruthchanakya80@gmail.com>
2026-09-02 08:21:37 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+ theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        manrope: ['Manrope', 'sans-serif'],
+        sans: ['"IBM Plex Sans"', 'sans-serif'],
+        mono: ['"JetBrains Mono"', 'monospace'],
+      },
+      borderRadius: {
